37bf47ee2dd0abdad077f7ca255a45d5ef9a432f
ishanrana363 <ishanrana363@gmail.com>
2024-10-16 20:11:30 +0600

    next js route done

---
+++ module-76/next-route/src/components/Navbar.jsx
@@ -0,0 +1,63 @@
+"use client"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+const Navbar = () => {
+    const userRouter = [
+        {
+            name: 'Home',
+            path: '/home',
+        },
+        {
+            name: 'About',
+            path: '/about',
+        },
+        {
+            name: 'Contact',
+            path: '/contact',
+        },
+        {
+            name: "Product",
+            path: '/product',
+        },
+        {
+            name: "Blog",
+            path: '/blog',
+        },
+        {
+            name: 'Login',
+            path: '/login',
+        },
+        {
+            name: 'Register',
+            path: '/register',
+        },
+    ];
+    const pathName = usePathname();
+    console.log(pathName);
+    const router = useRouter(); 
+    const handleChange = ()=>{
+        router.push("register")
+    }
+    return (
+        <div className='bg-green-400' >
+            <div className='max-w-screen-xl mx-auto ' >
+                <nav className='flex justify-between items-center  py-4 ' >
+                    <h1>NavLogo</h1>
+                    <ul className='flex gap-7  ' >
+                        {userRouter.map((router, index) => (
+                            <li key={index}>
+                                <Link className={`${pathName === router.path && "text-white underline "}`} href={router.path}>{router.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <button onClick={handleChange} className = "bg-gray-100 px-2 py-2 rounded-3xl  " >Registration</button>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
