49cbe5c10b0afe89828fcbe08a0919f8a23103a7
ishanrana363 <ishanrana363@gmail.com>
2024-10-18 21:16:33 +0600

    css module added

--- module-76/next-data-fetch/src/app/meals/page.jsx
+++ module-76/next-data-fetch/src/app/meals/page.jsx
@@ -1,11 +1,11 @@
 import Meal from '@/components/Meal'
 import React from 'react'
-
+import style from "./styles.module.css"
 const MealPage = () => {
     return (
         <div className='max-w-screen-xl mx-auto my-7 ' >
             <p className='font-semibold text-2xl text-center ' >Chose your meal</p>
-            <h1 className="font-bold text-center text-3xl " >Mead db</h1>
+            <h1 className= {style.font_green}  >Mead db</h1>
             {/* meal list */} 
             <Meal></Meal>
         </div>
